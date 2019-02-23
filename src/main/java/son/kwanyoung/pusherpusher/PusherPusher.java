56d9e6365564dbacd9496ea7bdc7697704394dae
kkwanyoung17 <kkwanyoung17@gmail.com>
2019-02-24 00:13:49 +0900

    Test reading csv file

--- src/main/java/son/kwanyoung/pusherpusher/PusherPusher.java
+++ src/main/java/son/kwanyoung/pusherpusher/PusherPusher.java
@@ -1,7 +1,24 @@
 package son.kwanyoung.pusherpusher;
 
+import de.siegmar.fastcsv.reader.*;
+import de.siegmar.fastcsv.writer.*;
+import java.io.*;
+import java.nio.charset.*;
+
 class PusherPusher {
     public static void main(String args[]) {
-        System.out.println("Hello PusherPusher!");
+        File file = new File("/home/kws/hellocsv.csv");
+        CsvReader csvReader = new CsvReader();
+
+        try {
+            CsvParser csvParser = csvReader.parse(file, StandardCharsets.UTF_8);
+            CsvRow row;
+            while((row = csvParser.nextRow()) != null) {
+                System.out.println("Read line: " + row);
+                System.out.println("First column of line: " + row.getField(0));
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
